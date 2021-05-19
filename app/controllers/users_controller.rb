43a74d8cf1b18586b62c309da87f5165ac99b1bb
Sean McNeil <sean.r.mcneil@gmail.com>
2021-05-19 13:39:49 -0600

    make users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show 
     @user = User.find(params[:id])
-    @articles = Article.where(user_id: @user.id)
+    @articles = @user.articles
+  end 
+
+  def index 
+    @users = User.all 
   end 
 
   def new
